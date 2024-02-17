c6a6a82b5eb071a1498f8c6296e29f2a75ee5ab0
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-02-18 00:10:19 +0100

    setup app.module.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { NgModule } from '@angular/core';
+import { routes } from './app/app.routes';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent, appConfig, )
   .catch((err) => console.error(err));
